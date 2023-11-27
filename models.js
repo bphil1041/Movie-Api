90a0a0511faea8c06b3068d360c7ab0c1b95aa63
bphil1041 <benjamin.r.phillips1041@gmail.com>
2023-11-27 13:07:17 -0600

    Update models.js

--- models.js
+++ models.js
@@ -1,31 +1,29 @@
-const mongoose = require('mongoose');
-
-let movieSchema = mongoose.Schema({
+et movieSchema = mongoose.Schema({
     Title: {type: String, required: true},
     Description: {type: String, required: true},
     Genre: {
-        Name: String,
-        Description: String
+      Name: String,
+      Description: String
     },
     Director: {
-        Name: String,
-        Bio: String
+      Name: String,
+      Bio: String
     },
     Actors: [String],
     ImagePath: String,
     Featured: Boolean
-});
-
-let userSchema = mongoose.Schema({
+  });
+  
+  let userSchema = mongoose.Schema({
     Username: {type: String, required: true},
-    Password: {tyle: String, required: true},
+    Password: {type: String, required: true},
     Email: {type: String, required: true},
     Birthday: Date,
-    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
-});
-
-let Movie = mongoose.model('Movie', movieSchema);
-let User = mongoose.model('User', userSchema);
-
-module.exports.Movie = Movie;
-module.exports.User = User;
+    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+  });
+  
+  let Movie = mongoose.model('Movie', movieSchema);
+  let User = mongoose.model('User', userSchema);
+  
+  module.exports.Movie = Movie;
+  module.exports.User = User;
