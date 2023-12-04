7db39db4db2a35f6033ffb2c9088742f7a3f7da6
bphil1041 <benjamin.r.phillips1041@gmail.com>
2023-12-04 10:12:46 -0600

    Update passport.js

--- passport.js
+++ passport.js
@@ -1,11 +1,11 @@
 const passport = require('passport'),
-  LocalStrategy = require('passport-local').Strategy,
-  Models = require('./models.js'),
-  passportJWT = require('passport-jwt');
+LocalStrategy = require('passport-local').Strategy,
+Models = require('./models.js'),
+passportJWT = require('passport-jwt');
 
 let Users = Models.User,
-  JWTStrategy = passportJWT.Strategy,
-  ExtractJWT = passportJWT.ExtractJwt;
+JWTStrategy = passportJWT.Strategy,
+ExtractJWT = passportJWT.ExtractJwt;
 
 passport.use(
     new LocalStrategy(
@@ -15,13 +15,17 @@ passport.use(
         },
         async (username, password, callback) => {
             console.log('${username} ${password}');
-            await Users.findOne({ Username: username})
-            .then ((user) => {
+            await Users.findOne({ Username: username })
+            .then((user) => {
                 if (!user) {
                     console.log('incorrect username');
                     return callback(null, false, {
-                        message: 'Incorrect username or password.',
+                        message: 'Incorrect username or password',
                     });
+                }
+                if (!user.validatePassword(password)) {
+                    console.log('incorrect password');
+                    return callback(null, false, { message: 'Incorrect password.' });
                 }
                 console.log('finished');
                 return callback(null, user);
@@ -36,6 +40,18 @@ passport.use(
     )
 );
 
+passport.use(new JWTStrategy({
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey: 'your_jwt_secret'
+}, async (jwtPayload, callback) => {
+    return await Users.findById(jwtPayload._id)
+        .then((user) => {
+            return callback(null, user);
+        })
+        .catch((error) => {
+            return callback(error)
+        });
+}));
 
 passport.use(new JWTStrategy({
     jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
