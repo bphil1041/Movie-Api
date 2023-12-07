b71da2b33ba51e5e067f6c21f8bb0bc2c4730c9e
bphil1041 <benjamin.r.phillips1041@gmail.com>
2023-12-07 08:14:48 -0600

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ const Movies = Models.Movie;
 const Users = Models.User;
 const { check, validationResult } = require('express-validator');
 
-mongoose.connect('mongodb://localhost:27017/myFLixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb://localhost:27017/myFLixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 const cors = require('cors');
