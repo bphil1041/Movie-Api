babb9dbbff62ac5f8f24dbdc1150ba746c8ac1b3
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-02-01 07:13:25 -0600

    Update index.js
    update CORS / allowed origins

--- index.js
+++ index.js
@@ -17,7 +17,18 @@ mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnified
 
 
 const cors = require('cors');
-app.use(cors());
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://myflix-bp.netlify.app', 'https://myflixbp-ee7590ef397f.herokuapp.com/'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 //Import auth
 let auth = require('./auth')(app);
