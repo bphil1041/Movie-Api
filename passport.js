fbb8663ea9d1b278b969972372ae0efa67f197eb
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-06-03 14:40:42 -0500

    Update passport.js

--- passport.js
+++ passport.js
@@ -1,3 +1,7 @@
+/**
+ * @fileoverview Passport strategies for user authentication.
+ */
+
 const passport = require('passport'),
   LocalStrategy = require('passport-local').Strategy,
   Models = require('./models.js'),
@@ -7,6 +11,15 @@ let Users = Models.User,
   JWTStrategy = passportJWT.Strategy,
   ExtractJWT = passportJWT.ExtractJwt;
 
+/**
+* Local strategy for username and password authentication.
+* @function
+* @name LocalStrategy
+* @param {Object} options - Options for the strategy.
+* @param {string} options.usernameField - Field name for the username.
+* @param {string} options.passwordField - Field name for the password.
+* @param {function} verify - Verification function.
+*/
 passport.use(
   new LocalStrategy(
     {
@@ -16,31 +29,37 @@ passport.use(
     async (username, password, callback) => {
       console.log(`${username} ${password}`);
       await Users.findOne({ Username: username })
-      .then((user) => {
-        if (!user) {
-          console.log('incorrect username');
-          return callback(null, false, {
-            message: 'Incorrect username or password.',
-          });
-        }
-        if (!user.validatePassword(password)) {
+        .then((user) => {
+          if (!user) {
+            console.log('incorrect username');
+            return callback(null, false, {
+              message: 'Incorrect username or password.',
+            });
+          }
+          if (!user.validatePassword(password)) {
             console.log('incorrect password');
-            return callback(null, false, { message: 'Incorrect password'});
-        }
-        console.log('finished');
-        return callback(null, user);
-      })
-      .catch((error) => {
-        if (error) {
-          console.log(error);
-          return callback(error);
-        }
-      })
+            return callback(null, false, { message: 'Incorrect password' });
+          }
+          console.log('finished');
+          return callback(null, user);
+        })
+        .catch((error) => {
+          if (error) {
+            console.log(error);
+            return callback(error);
+          }
+        })
     }
   )
 );
 
-
+/**
+ * JWT strategy for token authentication.
+ * @function
+ * @name JWTStrategy
+ * @param {Object} options - Options for the strategy.
+ * @param {function} verify - Verification function.
+ */
 passport.use(new JWTStrategy({
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
   secretOrKey: 'your_jwt_secret'
